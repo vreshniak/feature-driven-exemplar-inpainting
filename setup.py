637daa7c818d80ac5761adcd9168d3e74946689a
Viktor Reshniak <revitmt@gmail.com>
2020-08-13 23:26:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ ext_modules = Extension(name="patchmatch",
 
 # Note: setup() has access to cmd arguments of the setup.py script via sys.argv
 setup(name="pm",
-	ext_modules=cythonize(module_list=[ext_modules],force=True,annotate=False),
+	ext_modules=cythonize( module_list=[ext_modules], force=True, annotate=False, compiler_directives={'language_level' : "3"} ),
 	# py_modules=["inpainting", "features", "utils"],
 	version='0.1.0',
 	packages=["imgproc"],
